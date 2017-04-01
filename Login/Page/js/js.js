bbe4aaf37dbd01b8b5d3c28344cfc760f224731f
EntertainOBC <petervandao@icloud.com>
2017-04-01 09:12:12 -0700

    e

--- Login/Page/js/js.js
+++ Login/Page/js/js.js
@@ -1,17 +1,12 @@
-// Quick and Simple Tabs by Todd Gallimore
-// http://codepen.io/todd01925/pen/awGzD
-// A small adaptation by Ravenous
-
-$('.tabgroup > div').hide();
-$('.tabgroup > div:first-of-type').show();
-$('.tabs a').click(function(e){
-  e.preventDefault();
-    var $this = $(this),
-        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-        others = $this.closest('div').siblings().children('a'),
-        target = $this.attr('href');
-    others.parent().removeClass('active');
-    $this.parent().addClass('active');
-    $(tabgroup).children('div').hide();
-    $(target).show();
-})
+/**
+* very much a work in progress
+* HTML + CSS for a class demo in WIP400 at Platt College San Diego
+*
+*
+* @TODO:
+*  form inputs
+*  responsive tables
+*  more icons: trash
+*  matching login/register form
+*  toggle menus
+*/
