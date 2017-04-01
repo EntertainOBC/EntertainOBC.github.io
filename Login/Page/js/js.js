73926e526ca5f6478963c9cb97444157e448a20b
EntertainOBC <petervandao@icloud.com>
2017-04-01 09:00:45 -0700

    k

--- Login/Page/js/js.js
+++ Login/Page/js/js.js
@@ -1,8 +1,17 @@
-$('#menu-button').on('click', function() {
-  $('#nav').slideToggle();
-});
-$( window ).resize(function() {
-  if ($( window ).width() > 700) {
-    $('#nav').show();
-  }
-});
+// Quick and Simple Tabs by Todd Gallimore
+// http://codepen.io/todd01925/pen/awGzD
+// A small adaptation by Ravenous
+
+$('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('div').siblings().children('a'),
+        target = $this.attr('href');
+    others.parent().removeClass('active');
+    $this.parent().addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+})
