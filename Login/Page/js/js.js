49f729150b12d3189c85725520da638b0611f508
EntertainOBC <petervandao@icloud.com>
2017-04-24 18:36:55 -0700

    asdads

--- Login/Page/js/js.js
+++ Login/Page/js/js.js
@@ -1,11 +1,17 @@
-$(function() {
-  $('#main-navigation a').click(function() {
-    $('#main-navigation a').removeClass('active-tab');
-    $(this).addClass('active-tab');
-    $('#partial-title').text($(this).text());
-  });
-  $('#partial-navigation a').click(function() {
-    $('#partial-navigation a').removeClass('active-tab');
-    $(this).addClass('active-tab');
-  });
-});
+// Quick and Simple Tabs by Todd Gallimore
+// http://codepen.io/todd01925/pen/awGzD
+// A small adaptation by Ravenous
+
+$('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('div').siblings().children('a'),
+        target = $this.attr('href');
+    others.parent().removeClass('active');
+    $this.parent().addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+})
