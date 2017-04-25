a99569e655461a42b8b1d14127bf80a03bff0a8b
EntertainOBC <petervandao@icloud.com>
2017-04-24 18:48:19 -0700

    sadasd

--- Login/Page/js/js.js
+++ Login/Page/js/js.js
@@ -1,17 +1,11 @@
-// Quick and Simple Tabs by Todd Gallimore
-// http://codepen.io/todd01925/pen/awGzD
-// A small adaptation by Ravenous
-
-$('.tabgroup > div').hide();
-$('.tabgroup > div:first-of-type').show();
-$('.tabs a').click(function(e){
-  e.preventDefault();
-    var $this = $(this),
-        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-        others = $this.closest('div').siblings().children('a'),
-        target = $this.attr('href');
-    others.parent().removeClass('active');
-    $this.parent().addClass('active');
-    $(tabgroup).children('div').hide();
-    $(target).show();
-})
+class AdminPanelViewModel
+  constructor: () ->
+    @displayTab = ko.observable('users')
+    @collapsedNav = ko.observable(false)
+  changeTab: (tab) =>
+    @displayTab(tab)
+  collapse: () =>
+    @collapsedNav(!@collapsedNav())
+    
+$ ->
+  ko.applyBindings(new AdminPanelViewModel())
