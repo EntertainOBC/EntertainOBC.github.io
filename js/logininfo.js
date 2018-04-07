ced9e536f2650ae3b6f3ccb438ea042784ada935
EntertainOBC <entertainobc.github.io>
2018-04-07 16:38:52 -0700

    asdasdasd

---
+++ js/logininfo.js
@@ -0,0 +1,19 @@
+function inputFocus(i) {
+    if (i.value == i.defaultValue) { i.value = ""; i.style.color = "#000"; }
+}
+function inputBlur(i) {
+    if (i.value == "") { i.value = i.defaultValue; i.style.color = "#888"; }
+}
+var checkLoginPass = function () {
+    var login = document.getElementById("login").value;
+    var pass = document.getElementById("password").value;
+    if (login === "peter" && pass === "6761") {
+        window.location.replace("http://entertainobc.github.io/USERWEB/userkey=3937221/user=peter");
+    }
+    if (login === "bryan" && pass === "6631") {
+        window.location.replace("http://entertainobc.github.io/USERWEB/userkey=3937221/user=bryan");
+    }
+    else {
+        //do something else;
+    }
+}
